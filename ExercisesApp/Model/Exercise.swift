7c7473cd2f34a194b9590ad38dbf7542c4919029
Egor Olenburg <egor.olenburg1@gmail.com>
2020-09-16 23:03:47 +0900

    update Exercise Model

--- ExercisesApp/Model/Exercise.swift
+++ ExercisesApp/Model/Exercise.swift
@@ -10,12 +10,16 @@ import UIKit
 
 class Exercise: NSObject {
 
-    var id = 0
+    var id = ""
     var name = ""
     var desc = ""
     var imagePath = ""
     var gifPath = ""
     var videoPath = ""
+    var creators = ""
+    var equipment = ""
+    var primary_muscle = ""
+    var secondary_muscle = ""
 
     
     override init() {
@@ -24,6 +28,16 @@ class Exercise: NSObject {
     
     init(_ json: [String: Any]) {
         
+        id = json["id"] as? String ?? ""
+        name = json["name"] as? String ?? ""
+        desc = json["description"] as? String ?? ""
+        imagePath = json["image"] as? String ?? ""
+        gifPath = json["gif"] as? String ?? ""
+        videoPath = json["video"] as? String ?? ""
+        creators = json["creators"] as? String ?? ""
+        equipment = json["equipment"] as? String ?? ""
+        primary_muscle = json["primary_muscle"] as? String ?? ""
+        secondary_muscle = json["secondary_muscle"] as? String ?? ""
 
     }
 }
